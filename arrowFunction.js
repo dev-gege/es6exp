5d3092250471ac99a8b3550a5abf7744e2ec7397
dev-gege <96869379+dev-gege@users.noreply.github.com>
2023-04-09 12:48:12 +0300

    Create arrowFunction.js

---
+++ arrowFunction.js
@@ -0,0 +1,11 @@
+let createSomething = function(message, name) {
+  return message + name; 
+}
+
+// Same function with arrow
+let arrowSomething = (message, name) => {
+  return message + name 
+}
+
+// One liner arrow function
+let arrowSomething = message, name => message + name 
