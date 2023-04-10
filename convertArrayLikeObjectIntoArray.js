8ff667ab8321cdf4d62099f3dff4ae23801bbd5d
dev-gege <96869379+dev-gege@users.noreply.github.com>
2023-04-10 08:30:51 +0300

    Create convertArrayLikeObjectIntoArray.js

---
+++ convertArrayLikeObjectIntoArray.js
@@ -0,0 +1,15 @@
+/*
+<ul>
+  <li class="list">16.99</li>
+  <li class="list">6.99</li>
+  <li class="list">5.66</li>
+</ul>
+*/
+const list = Array.from(document.querySelectorAll('.list'))
+
+list
+  .filter(item => parseFloat(list.innerHTML)
+  .forEach(item => item.style.color = 'blue')
+          
+console.log(list)
+ 
